b2b5367ce5ddcf5c92552a95476dcfe07288e4be
Sopiha Mykola <niksop94@gmail.com>
2017-09-18 19:28:41 +0300

    new commit

--- approver.php
+++ approver.php
@@ -2,55 +2,115 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Results</title>
+	<link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
 </head>
 <body>
 
-<?php
 
-if ($_FILES['fb_data']['size'] != 0 && $_FILES['tl_data']['size'] != 0)
-	if (isset($_SERVER['HTTP_REFERER']))
-		header("Location: ".$_SERVER['HTTP_REFERER']);
-else
-	header("Location: /");
+	<?php
+		if ($_FILES['fb_data']['size'] != 0 && $_FILES['tl_data']['size'] != 0 && $_POST['rub_curr'] != "")
+			if (isset($_SERVER['HTTP_REFERER']))
+				header("Location: ".$_SERVER['HTTP_REFERER']);
+		else
+			header("Location: /");
 
-$fb_file = fopen ("fb_data.csv","r");
-$tl_file = fopen ("tl_data.csv","r");
+		$fb_file = fopen ("fb_data.csv","r");
+		$tl_file = fopen ("tl_data.csv","r");
+		$rub_curr = floatval($_POST['rub_curr']);
 
-fgetcsv ($fb_file, 1000, ",");
-fgetcsv ($tl_file, 1000, ",");
+		fgetcsv ($fb_file, 1000, ",");
+		fgetcsv ($tl_file, 1000, ",");
 
-$fb_campaigns = array();
-$tl_campaigns = array();
+		$fb_campaigns = array();
+		$tl_campaigns = array();
+		$results = array();
 
-while ($data = fgetcsv ($fb_file, 1000, ",")) {
-	$campaign = $data[2];
-	$stream = substr($campaign, strrpos($campaign, '-s') + 2);
-	$cost = $data[7];
-	$repotr_date = $data[1];
+		// Facebook data
+		while ($data = fgetcsv ($fb_file, 1000, ",")) {
+			if ($data[2] != '') {
+				$campaign = $data[2];
+				$stream = substr($campaign, strrpos($campaign, '-s') + 2);
+				if (strlen($data[7]) == 0) {
+					$cost = '0';
+				} else {
+					$cost = $data[7];
+				}
+				$report_date = $data[1];
+				if ($stream != '') {
+					$fb_campaigns[$stream][$report_date] = floatval($cost);
+					$results[$stream][$report_date]['cost'] = round(floatval($cost)/$rub_curr, 2);
+					$results[$stream][$report_date]['price'] = 0;
+				}
+			}
+		}
 
-	if ($stream != '') {
-		$fb_campaigns[$stream]['costs'][] = $cost;
-		$fb_campaigns[$stream]['repotr_date'][] = $repotr_date;
-	}
-}
+		// TerraLeads data
+		while ($data = fgetcsv ($tl_file, 1000, ",")) {
+			$product = $data[6];
+			$stream = $data[12];
+			$price = $data[13];
+			$lead_time = substr($data[2], 0, 10);
+			if ($stream != '' && array_key_exists($stream, $fb_campaigns)) {
+				$tl_campaigns[$stream][$lead_time] = floatval($price);
+				$results[$stream][$lead_time]['price'] = floatval($price);
+			}
+		}
 
-while ($data = fgetcsv ($tl_file, 1000, ",")) {	
-	$stream = $data[12].str;
-	$price = $data[13];
-	$lead_time = $data[2];
+		echo "
+	<table id=\"example\">
+		<thead>
+			<tr>
+				<th class=\"site_name\">Stream ID</th>
+				<th>Day</th>
+				<th>Cost</th>
+				<th>Price</th>
+				<th>Profit</th>
+				<th>ROI</th>
+			</tr>
+		</thead>
+	<tbody>
+	";
 
-	if ($stream != '') {
-		$tl_campaigns[$stream]['price'][] = $price;
-		$tl_campaigns[$stream]['lead_time'][] = $lead_time;
-	}
-}
+		$keys = array_keys($results);
+		foreach ($keys as &$key) {
+			$data = array_keys($results[$key]);
+			// echo "<tr>";
+			foreach ($data as &$day) {
+				echo "<tr>";
+				$results[$key][$day]['profit'] = $results[$key][$day]['price'] - $results[$key][$day]['cost'];
+				$results[$key][$day]['roi'] = ($results[$key][$day]['cost'] == 0) ? '--' :
+					round(($results[$key][$day]['price'] - $results[$key][$day]['cost'])/$results[$key][$day]['cost']*100, 2).'%';
+				echo "<td>".$key."</td>";
+				echo "<td>".$day."</td>";
+				echo "<td>".$results[$key][$day]['cost']."</td>";
+				echo "<td>".$results[$key][$day]['price']."</td>";
+				echo "<td>".$results[$key][$day]['profit']."</td>";
+				echo "<td>".$results[$key][$day]['roi']."</td>";
+				echo "</tr>";
+			}
+			// echo "</tr>";
+		}
 
-print_r($tl_campaigns);
+		echo "
+    </tbody>
+  </table>
+		";
 
-fclose($fb_file);
-fclose($tl_file);
-?>
+		//print_r($results);
 
+		fclose($fb_file);
+		fclose($tl_file);
+	?>
+
+
+	<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
+	<script>
+		$(function(){
+			$("#example").dataTable();
+		})
+	</script>
 </body>
 </html>
+
--- index.php
+++ index.php
@@ -3,19 +3,35 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Approver</title>
+	<style type="text/css">
+		* {
+			box-sizing: border-box;
+		}
+		html, body {
+			height: 100%;
+			margin: 0;
+			padding: 0;
+		}
+		.container {
+			display: flex;
+			height: 100%;
+		}
+		.container form {
+			margin: auto;
+		}
+	</style>
 </head>
 <body>
 
-<!-- Тип кодирования данных, enctype, ДОЛЖЕН БЫТЬ указан ИМЕННО так -->
-<form enctype="multipart/form-data" action="approver.php" method="POST">
-	<!-- Поле MAX_FILE_SIZE должно быть указано до поля загрузки файла -->
-	<input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-	<!-- Название элемента input определяет имя в массиве $_FILES -->
-	Отправить FB файл: <input name="fb_data" type="file" />
-	<!-- Название элемента input определяет имя в массиве $_FILES -->
-	Отправить TL файл: <input name="tl_data" type="file" />
-	<input type="submit" value="Send File" />
-</form>
+	<div class="container">
+		<form enctype="multipart/form-data" action="approver.php" method="POST">
+			<input type="hidden" name="MAX_FILE_SIZE" value="30000" /><br /><br />
+			Отправить FB файл: <input name="fb_data" type="file" /><br /><br />
+			Отправить TL файл: <input name="tl_data" type="file" /><br /><br />
+			Курс рубля к доллару <input name="rub_curr" type="text" /><br /><br />
+			<input type="submit" value="Approve!" />
+		</form>
+	</div>
 
 </body>
 </html>
